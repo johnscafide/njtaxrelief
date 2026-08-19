993a43e80fede390f254a388cf1c56e022e07c26
John <152080052+johnscafide@users.noreply.github.com>
2026-08-19 12:01:08 -0400

    Wire Watchdog Zapier integration

---
+++ integrations/zapier/index.js
@@ -0,0 +1,32 @@
+const authentication = require('./authentication');
+const { addAuth } = require('./common');
+
+const propertySignalChanged = require('./triggers/property-signal-changed');
+const watchlistAlert = require('./triggers/watchlist-alert');
+const reportReady = require('./triggers/report-ready');
+const intelligenceFindingCreated = require('./triggers/intelligence-finding-created');
+const findProperty = require('./searches/find-property');
+const getPropertySnapshot = require('./searches/get-property-snapshot');
+const addToWatchlist = require('./creates/add-to-watchlist');
+const attachCrmContext = require('./creates/attach-crm-context');
+
+module.exports = {
+  version: require('./package.json').version,
+  platformVersion: require('zapier-platform-core').version,
+  authentication,
+  beforeRequest: [addAuth],
+  triggers: {
+    [propertySignalChanged.key]: propertySignalChanged,
+    [watchlistAlert.key]: watchlistAlert,
+    [reportReady.key]: reportReady,
+    [intelligenceFindingCreated.key]: intelligenceFindingCreated,
+  },
+  searches: {
+    [findProperty.key]: findProperty,
+    [getPropertySnapshot.key]: getPropertySnapshot,
+  },
+  creates: {
+    [addToWatchlist.key]: addToWatchlist,
+    [attachCrmContext.key]: attachCrmContext,
+  },
+};
