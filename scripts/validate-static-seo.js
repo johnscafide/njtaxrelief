f74a23afa9b01db6aaa26549b672ca071be73215
John <152080052+johnscafide@users.noreply.github.com>
2026-08-25 18:53:24 -0400

    Guard NJ revaluation statistics sitemap route

--- scripts/validate-static-seo.js
+++ scripts/validate-static-seo.js
@@ -118,7 +118,8 @@ const expectedClusters = {
     'https://www.watchdogindex.com/statistics/nj-property-tax-appeal-win-rates-by-county-2026',
     'https://www.watchdogindex.com/statistics/least-uniform-property-assessments-new-jersey-2026',
     'https://www.watchdogindex.com/statistics/nj-school-tax-levy-share-by-town-2026',
-    'https://www.watchdogindex.com/statistics/biggest-property-tax-rate-increases-new-jersey-2026'
+    'https://www.watchdogindex.com/statistics/biggest-property-tax-rate-increases-new-jersey-2026',
+    'https://www.watchdogindex.com/statistics/nj-revaluation-list-2026'
   ]
 };
 
