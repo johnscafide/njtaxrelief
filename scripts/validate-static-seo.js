b8c395c4fa824a798505331452aab0d69543f68d
John <152080052+johnscafide@users.noreply.github.com>
2026-08-25 15:52:27 -0400

    NJW-131 guard assessment uniformity statistics route

--- scripts/validate-static-seo.js
+++ scripts/validate-static-seo.js
@@ -115,7 +115,8 @@ const expectedClusters = {
     'https://www.watchdogindex.com/statistics/nj-property-tax-rates-by-town-2026',
     'https://www.watchdogindex.com/statistics/highest-property-tax-rates-new-jersey-2026',
     'https://www.watchdogindex.com/statistics/lowest-property-tax-rates-new-jersey-2026',
-    'https://www.watchdogindex.com/statistics/nj-property-tax-appeal-win-rates-by-county-2026'
+    'https://www.watchdogindex.com/statistics/nj-property-tax-appeal-win-rates-by-county-2026',
+    'https://www.watchdogindex.com/statistics/least-uniform-property-assessments-new-jersey-2026'
   ]
 };
 
