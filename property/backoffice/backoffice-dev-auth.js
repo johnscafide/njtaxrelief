3ffd6d14144401135b751dc7a89b22205e838f54
John <152080052+johnscafide@users.noreply.github.com>
2026-09-15 17:33:03 -0400

    NJW-353 point Backoffice badge to developer review API

--- property/backoffice/backoffice-dev-auth.js
+++ property/backoffice/backoffice-dev-auth.js
@@ -2,7 +2,7 @@
 'use strict';
 const SESSION_API='/api/watchdog-backoffice-gateway?target=login';
 const BACKOFFICE_API='/api/watchdog-backoffice-gateway?target=api';
-const REVIEWS_API='/api/watchdog-backoffice-gateway?target=reviews';
+const REVIEWS_API='/api/watchdog-backoffice-reviews';
 const LEGACY_BACKOFFICE_API='https://uvkvaxljhhngydvlrzom.supabase.co/functions/v1/backoffice-api';
 const SESSION_KEY='watchdog-backoffice-session';
 const $=(s,r=document)=>r.querySelector(s);
